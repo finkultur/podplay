ff4c07d210339fd3af4fd6f1b9fc320eaec0bdb8
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-01-25 20:09:21 +0100

    Added --seek <position>

--- podplay.py
+++ podplay.py
@@ -6,6 +6,7 @@ import requests
 import urllib
 import podcastparser
 import vlc
+import time
 
 import pprint
 
@@ -37,12 +38,49 @@ def print_ep(pod, num=0):
 
 # Simply plays an episode
 # TODO: Implement pause
-def play_ep(pod, num=0):
-    mp3 = pod['episodes'][num]['enclosures'][0]['url']
-    p = vlc.MediaPlayer(mp3)
-    p.play()
-    while True:
-        continue
+def play_ep(pod, num=0, pos_str=""):
+    audio_url = pod['episodes'][num]['enclosures'][0]['url']
+    dur = pod['episodes'][num]['total_time']
+    pos = parse_pos(pos_str, dur)
+    if pos >= 1.0: return
+
+    instance = vlc.Instance()
+    player = instance.media_player_new()
+    media = instance.media_new(audio_url)
+    player.set_media(media)
+    # Apparently VLC wont seek to position if we do not play, pause, set & play
+    player.play()
+    player.pause()
+    player.set_position(pos)
+    player.play()
+    time.sleep(dur-pos*dur)
+
+# Parses a position string to a float. Input may be:
+# A percentage such as "12%"
+# A time such as "hh:mm:ss" or "m:ss"
+# A number of seconds such as "123"
+def parse_pos(pos_str, total_dur):
+    pos = 0.0
+    # If given a percentage
+    if pos_str.endswith('%'):
+        try:
+            return float(pos_str.strip('%'))/100
+        except ValueError:
+            print("Invalid input, starting podcast from beginning")
+            return 0.0
+    # If given a string such as hh:mm:ss or m:ss, calculate total seconds
+    elif ':' in pos_str:
+        try:
+            # hh:mm:ss to seconds
+            for s in pos_str.split(':'):
+                pos = pos*60 + int(s)
+        except ValueError:
+            print("Invalid input, starting podcast from beginning")
+            return 0.0
+    # Convert seconds to percentage
+    return float(pos)/total_dur
+
+
 
 # Since all feeds are latest first, this is done
 # -x = x:th latest
@@ -62,6 +100,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("podcast")
     parser.add_argument("-e", "--episode", type=int, default=0, help="Choose a specific episode")
+    parser.add_argument("-s", "--seek", type=str, default="",
+                        help="Seek to given (seconds or hh:mm:ss or percentage) position")
     parser.add_argument("-i", "--info", action="store_true",
                         help="Only print information, do not play")
     args = parser.parse_args()
@@ -70,5 +110,5 @@ if __name__ == "__main__":
     ep = get_correct_ep_num(pod, args.episode)
     print_ep(pod, ep)
     if not args.info:
-        play_ep(pod, ep)
+        play_ep(pod, ep, args.seek)
 
