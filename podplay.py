56d529f3e4aa0950c79dab3ce1d398809082b0a1
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-01-26 03:48:02 +0100

    Fixed play/pause, uses curses

--- podplay.py
+++ podplay.py
@@ -1,102 +1,66 @@
 #!/usr/bin/env python
-import sys
+# -*- coding: utf-8 -*-
+
 import argparse
-import datetime
-import requests
-import urllib
-import podcastparser
 import vlc
 import time
+import curses
+import locale
 
-import pprint
+from utils import *
 
-ITUNES_URL = "https://itunes.apple.com/search"
+# Init VLC player and instance
+def init_player():
+    global player, instance
+    instance = vlc.Instance()
+    player = instance.media_player_new()
 
-# Returns the feed url using the itunes search api. Simply takes the first hit.
-def get_feed_url(search_str):
-    payload = {'term': search_str, 'media': 'podcast'}
-    r = requests.get(ITUNES_URL, params=payload)
-    return r.json()['results'][0]['feedUrl']
-
-# Parses a feed_url to a dict
-def get_pod(feed_url):
-    return podcastparser.parse(feed_url, urllib.urlopen(feed_url))
-
-# Pretty prints episode information
-def print_ep(pod, num=0):
-    num_eps = len(pod['episodes'])
-    ep = pod['episodes'][num]
-    time = ep['total_time'] / 60
-    date = datetime.datetime.fromtimestamp(int(
-            ep['published'])
-           ).strftime("%Y-%m-%d %H:%M:%S")
-    print(pod['title'])
-    print(ep['title'])
-    if (ep['description'] != ""): print(ep['description'])
-    print(date)
-    print(str(time) + " minutes")
-
-# Simply plays an episode
-# TODO: Implement pause
-def play_ep(pod, num=0, pos_str=""):
+# Set podcast episode and initial position
+def set_ep(pod, num=0, pos_str=""):
+    global player, instance
     audio_url = pod['episodes'][num]['enclosures'][0]['url']
     dur = pod['episodes'][num]['total_time']
     pos = parse_pos(pos_str, dur)
-    if pos >= 1.0: return
 
-    instance = vlc.Instance()
-    player = instance.media_player_new()
     media = instance.media_new(audio_url)
     player.set_media(media)
-    # Apparently VLC wont seek to position if we do not play, pause, set & play
-    player.play()
-    player.pause()
     player.set_position(pos)
+
+# Wrapper for our wrapper :D
+def curses_wraps(fn):
+    return lambda *args: curses.wrapper(fn, *args)
+
+# Main event loop
+@curses_wraps
+def cli(win, args):
+    global player
+    pod = get_pod(get_feed_url(args.podcast))
+    ep = get_correct_ep_num(pod, args.episode)
+    init_player()
+    set_ep(pod, ep, args.seek)
+
     player.play()
-    time.sleep(dur-pos*dur)
-
-# Parses a position string to a float. Input may be:
-# A percentage such as "12%"
-# A time such as "hh:mm:ss" or "m:ss"
-# A number of seconds such as "123"
-def parse_pos(pos_str, total_dur):
-    pos = 0.0
-    # If given a percentage
-    if pos_str.endswith('%'):
-        try:
-            return float(pos_str.strip('%'))/100
-        except ValueError:
-            print("Invalid input, starting podcast from beginning")
-            return 0.0
-    # If given a string such as hh:mm:ss or m:ss, calculate total seconds
-    elif ':' in pos_str:
-        try:
-            # hh:mm:ss to seconds
-            for s in pos_str.split(':'):
-                pos = pos*60 + int(s)
-        except ValueError:
-            print("Invalid input, starting podcast from beginning")
-            return 0.0
-    # Convert seconds to percentage
-    return float(pos)/total_dur
-
-
-
-# Since all feeds are latest first, this is done
-# -x = x:th latest
-# 0 = latest released
-# x = x:th episode
-def get_correct_ep_num(pod, num):
-    if num == 0: return 0
-    num_eps = len(pod['episodes'])
-    if num > 0: return num_eps-num
-    if num < 0: return abs(num)
+    paused = False
  
-# Usage:
-# podplay "string": Play latest episode of first hit
-# podplay -ep <x> "string": Play x:th episode
-# podplay -ep <-x> "string": Play x:th latest episode
+    #win.nodelay(True)
+    key=""
+    while 1:
+        win.clear()
+        win.addstr(print_ep(pod, 0))
+        if paused:
+            win.insstr(5, 0, " ===== PAUSE =====")
+        try:
+            key = win.getkey()
+            if str(key) == ' ':
+                player.pause()
+                paused = not paused
+        except Exception as e:
+            pass
+
+# Main
 if __name__ == "__main__":
+    locale.setlocale(locale.LC_ALL, '')
+
     parser = argparse.ArgumentParser()
     parser.add_argument("podcast")
     parser.add_argument("-e", "--episode", type=int, default=0, help="Choose a specific episode")
@@ -106,9 +70,8 @@ if __name__ == "__main__":
                         help="Only print information, do not play")
     args = parser.parse_args()
 
-    pod = get_pod(get_feed_url(args.podcast))
-    ep = get_correct_ep_num(pod, args.episode)
-    print_ep(pod, ep)
-    if not args.info:
-        play_ep(pod, ep, args.seek)
+    try:
+        cli(args)
+    except KeyboardInterrupt:
+        pass
 
---
+++ utils.py
@@ -0,0 +1,69 @@
+import requests
+import urllib
+import podcastparser
+import datetime
+
+ITUNES_URL = "https://itunes.apple.com/search"
+
+# Returns the feed url using the itunes search api. Simply takes the first hit.
+def get_feed_url(search_str):
+    payload = {'term': search_str, 'media': 'podcast'}
+    r = requests.get(ITUNES_URL, params=payload)
+    return r.json()['results'][0]['feedUrl']
+
+# Parses a feed_url to a dict
+def get_pod(feed_url):
+    return podcastparser.parse(feed_url, urllib.urlopen(feed_url))
+
+# Pretty prints episode information
+def print_ep(pod, num=0):
+    num_eps = len(pod['episodes'])
+    ep = pod['episodes'][num]
+    time = ep['total_time'] / 60
+    date = datetime.datetime.fromtimestamp(int(
+            ep['published'])
+           ).strftime("%Y-%m-%d %H:%M:%S")
+
+    string = pod['title'].encode("utf-8") + '\n'
+    string = str(ep['title']) + '\n'
+    if (ep['description'] != ""): 
+        string += ep['description'].encode("utf-8") + '\n'
+    string += date + '\n'
+    string += str(time) + " minutes"
+    return string
+
+# Parses a position string to a float. Input may be:
+# A percentage such as "12%"
+# A time such as "hh:mm:ss" or "m:ss"
+# A number of seconds such as "123"
+def parse_pos(pos_str, total_dur):
+    pos = 0.0 
+    # If given a percentage
+    if pos_str.endswith('%'):
+        try:
+            return float(pos_str.strip('%'))/100
+        except ValueError:
+            print("Invalid input, starting podcast from beginning")
+            return 0.0 
+    # If given a string such as hh:mm:ss or m:ss, calculate total seconds
+    elif ':' in pos_str:
+        try:
+            # hh:mm:ss to seconds
+            for s in pos_str.split(':'):
+                pos = pos*60 + int(s)
+        except ValueError:
+            print("Invalid input, starting podcast from beginning")
+            return 0.0
+    # Convert seconds to percentage
+    return float(pos)/total_dur
+
+# Since all feeds are latest first, this is done
+# -x = x:th latest
+# 0 = latest released
+# x = x:th episode
+def get_correct_ep_num(pod, num):
+    if num == 0: return 0
+    num_eps = len(pod['episodes'])
+    if num > 0: return num_eps-num
+    if num < 0: return abs(num)
+
