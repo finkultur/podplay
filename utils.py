0401eff914376f4ec49456c848a15a581dde19e1
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-01-31 19:57:53 +0100

    added get_pods()

--- utils.py
+++ utils.py
@@ -15,6 +15,16 @@ def get_feed_url(search_str):
 def get_pod(feed_url):
     return podcastparser.parse(feed_url, urllib.urlopen(feed_url))
 
+# Returns a list of parsed pod-dicts
+def get_pods(search_str):
+    payload = {'term': search_str, 'media': 'podcast'}
+    r = requests.get(ITUNES_URL, params=payload)
+    pods = []
+    for p in r.json()['results']:
+        feed_url = p['feedUrl']
+        pods.append(podcastparser.parse(feed_url, urllib.urlopen(feed_url)))
+    return pods
+
 # Pretty prints episode information
 def print_ep(pod, num=0):
     num_eps = len(pod['episodes'])
