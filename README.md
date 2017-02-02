dea6511767023defd478bac5ee3c56589fa46657
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-02-02 18:46:21 +0100

    Added setup.py

---
+++ README.md
@@ -0,0 +1,1 @@
+Python Podcast Player
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Podplay',
+      version='0.1',
+      description='Python Podcast Player',
+      author='Viktor Nilsson',
+      author_email='viktor.w.nilsson@gmail.com',
+      url='https://github.com/finkultur/podplay',
+      py_modules=['podplay', 'utils'],
+     )
